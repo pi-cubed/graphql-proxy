a3c220afd121e3d839fdc119bd91f41a615b3735
Dylan Richardson <dylanrichardson1996@gmail.com>
2018-06-24 19:30:59 -0400

    fix: Add test mutation to server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ import ProxyServer from './index';
 const port = process.env.PORT || 5000;
 
 new ProxyServer({
-    typeDefs: `type Query { test: String }`,
-    resolvers: { Query: { test: () => 'test' } }
+    typeDefs: `
+    	type Query { test: String }
+    	type Mutation { test: String }
+    `,
+    resolvers: {
+        Query: { test: () => 'test' },
+        Mutation: { test: () => 'test' }
+    }
 }).start({ port }, () => console.log(`Proxy server started at ${port}.`));
